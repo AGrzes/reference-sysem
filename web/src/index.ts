9b96735a18d6a0e09783e61a711e12b84a8bd5fa
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2019-11-02 21:26:22 +0100

    Implemented navigation. Resolves #1

--- web/src/index.ts
+++ web/src/index.ts
@@ -14,19 +14,59 @@ Vue.component('book-summary', {
 
 const api = new Api('/api/books/')
 
+const containerWrapper = (content) => `
+<div class="col-9">
+  ${content}
+</div>
+`
+
 app({router: router([
-  listHandler({name: 'book', fetch: api.list('book')}),
-  itemHandler({name: 'book', fetch: api.get('book')}),
-  listHandler({name: 'author', fetch: api.list('author')}),
-  itemHandler({name: 'author', fetch: api.get('author')}),
-  listHandler({name: 'series', listName: 'series', fetch: api.list('series')}),
-  itemHandler({name: 'series', fetch: api.get('series')}),
-  listHandler({name: 'read', listName: 'read', fetch: api.list('read')}),
-  itemHandler({name: 'read', fetch: api.get('read')}),
-  listHandler({name: 'owned', listName: 'owned', fetch: api.list('owned')}),
-  itemHandler({name: 'owned', fetch: api.get('owned')}),
-  listHandler({name: 'wanted', listName: 'wanted', fetch: api.list('wanted')}),
-  itemHandler({name: 'wanted', fetch: api.get('wanted')})
-])})
+  {
+    name: 'root',
+    path: '/',
+    component: {
+      template: `
+<div class="container mt-3">
+  <router-view></router-view>
+</div>
+      `
+    },
+    children: [{
+      name: 'books-system',
+      path: 'books',
+      component: {
+        template: `
+<div class="row">
+  <div class="col-3">
+    <div class="nav flex-column nav-pills">
+      <router-link :to="{name:'book-list'}" class="nav-link">Books</router-link>
+      <router-link :to="{name:'author-list'}" class="nav-link">Authors</router-link>
+      <router-link :to="{name:'series-list'}" class="nav-link">Series</router-link>
+      <router-link :to="{name:'owned-list'}" class="nav-link">Owned</router-link>
+      <router-link :to="{name:'wanted-list'}" class="nav-link">Wanted</router-link>
+      <router-link :to="{name:'read-list'}" class="nav-link">Read</router-link>
+    </div>
+  </div>
+  <router-view></router-view>
+</div>
+        `
+      },
+      children: [
+        listHandler({name: 'book', fetch: api.list('book'), containerWrapper}),
+        itemHandler({name: 'book', fetch: api.get('book'), containerWrapper}),
+        listHandler({name: 'author', fetch: api.list('author'), containerWrapper}),
+        itemHandler({name: 'author', fetch: api.get('author'), containerWrapper}),
+        listHandler({name: 'series', listName: 'series', fetch: api.list('series'), containerWrapper}),
+        itemHandler({name: 'series', listName: 'series', fetch: api.get('series'), containerWrapper}),
+        listHandler({name: 'read', listName: 'read', fetch: api.list('read'), containerWrapper}),
+        itemHandler({name: 'read', listName: 'read', fetch: api.get('read'), containerWrapper}),
+        listHandler({name: 'owned', listName: 'owned', fetch: api.list('owned'), containerWrapper}),
+        itemHandler({name: 'owned', listName: 'owned', fetch: api.get('owned'), containerWrapper}),
+        listHandler({name: 'wanted', listName: 'wanted', fetch: api.list('wanted'), containerWrapper}),
+        itemHandler({name: 'wanted', listName: 'wanted', fetch: api.get('wanted'), containerWrapper})
+      ]
+    }
+  ]
+}])})
 
 bootstrap()
