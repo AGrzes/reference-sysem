1b259260bb34fcbbf30b7c7f839214d9d758b9cb
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2019-11-03 07:43:36 +0100

    Model exploraton components, resolves #3

--- web/src/index.ts
+++ web/src/index.ts
@@ -7,12 +7,157 @@ Vue.component('book-summary', {
 <div class="card">
   <div class="card-body">
     <h5 class="card-title">{{book.name}} <small>{{book.author}}</small></h5>
+    <router-link :to="{name:'book',params:{book:book._id}}" class="nav-link">Details</router-link>
   </div>
 </div>
   `
 })
 
-const api = new Api('/api/books/')
+Vue.component('book-details', {
+  props: ['book'],
+  template:  `
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">{{book.name}} <small>{{book.author}}</small></h5>
+    <p>{{book.description}}</p>
+    <span class="badge badge-primary mr-1" v-for="label in book.labels">{{label}}</span>
+  </div>
+</div>
+  `
+})
+
+Vue.component('author-summary', {
+  props: ['author'],
+  template:  `
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">{{author.name}}</h5>
+    <router-link :to="{name:'author',params:{author:author._id}}" class="nav-link">Details</router-link>
+  </div>
+</div>
+  `
+})
+
+Vue.component('author-details', {
+  props: ['author'],
+  template:  `
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">{{author.name}}</h5>
+    <p>{{author.description}}</p>
+    <ul>
+      <router-link :to="{name:'book',params:{book}}" tag="li" v-for="book in author.books">book</router-link>
+    </ul>
+  </div>
+</div>
+  `
+})
+
+Vue.component('series-summary', {
+  props: ['series'],
+  template:  `
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">{{series.name}}</h5>
+    <router-link :to="{name:'series',params:{series:series._id}}" class="nav-link">Details</router-link>
+  </div>
+</div>
+  `
+})
+
+Vue.component('series-details', {
+  props: ['series'],
+  template:  `
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">{{series.name}}</h5>
+    <p>{{series.description}}</p>
+    <ul>
+      <router-link :to="{name:'book',params:{book}}" tag="li" v-for="book in series.books">book</router-link>
+    </ul>
+  </div>
+</div>
+  `
+})
+
+Vue.component('owned-summary', {
+  props: ['owned'],
+  template:  `
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">{{owned.book}}</h5>
+    <router-link :to="{name:'owned',params:{owned:owned._id}}" class="nav-link">Details</router-link>
+  </div>
+</div>
+  `
+})
+
+Vue.component('owned-details', {
+  props: ['owned'],
+  template:  `
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title"><router-link :to="{name:'book',params:{book: owned.book}}">{{owned.book}}</router-link></h5>
+    <p>{{owned.description}}</p>
+    <span class="badge badge-primary mr-1">{{owned.form}}</span>
+    <span class="badge badge-primary mr-1">{{owned.library}}</span>
+  </div>
+</div>
+  `
+})
+
+Vue.component('wanted-summary', {
+  props: ['wanted'],
+  template:  `
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">{{wanted.book}}</h5>
+    <router-link :to="{name:'wanted',params:{wanted:wanted._id}}" class="nav-link">Details</router-link>
+  </div>
+</div>
+  `
+})
+
+Vue.component('wanted-details', {
+  props: ['wanted'],
+  template:  `
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title"><router-link :to="{name:'book',params:{book: wanted.book}}">{{wanted.book}}</router-link></h5>
+    <p>{{wanted.description}}</p>
+  </div>
+</div>
+  `
+})
+
+Vue.component('read-summary', {
+  props: ['read'],
+  template:  `
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">{{read.book}}</h5>
+    <router-link :to="{name:'read',params:{read:read._id}}" class="nav-link">Details</router-link>
+  </div>
+</div>
+  `
+})
+
+Vue.component('read-details', {
+  props: ['read'],
+  template:  `
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title"><router-link :to="{name:'book',params:{book: read.book}}">{{read.book}}</router-link></h5>
+    <p>{{read.description}}</p>
+    <ul>
+      <li v-for="progress in read.progress">{{progress.date}} page {{progress.position}} <span class="badge badge-primary mr-1">+{{progress.increment}}</span></li>
+    </ul>
+  </div>
+</div>
+  `
+})
+
+const api = new Api('/api/books/', false)
 
 const containerWrapper = (content) => `
 <div class="col-9">
@@ -52,10 +197,10 @@ app({router: router([
         `
       },
       children: [
-        listHandler({name: 'book', fetch: api.list('book'), containerWrapper}),
-        itemHandler({name: 'book', fetch: api.get('book'), containerWrapper}),
-        listHandler({name: 'author', fetch: api.list('author'), containerWrapper}),
-        itemHandler({name: 'author', fetch: api.get('author'), containerWrapper}),
+        listHandler({name: 'book', fetch: api.list('books'), containerWrapper}),
+        itemHandler({name: 'book', fetch: api.get('books'), containerWrapper}),
+        listHandler({name: 'author', fetch: api.list('authors'), containerWrapper}),
+        itemHandler({name: 'author', fetch: api.get('authors'), containerWrapper}),
         listHandler({name: 'series', listName: 'series', fetch: api.list('series'), containerWrapper}),
         itemHandler({name: 'series', listName: 'series', fetch: api.get('series'), containerWrapper}),
         listHandler({name: 'read', listName: 'read', fetch: api.list('read'), containerWrapper}),
