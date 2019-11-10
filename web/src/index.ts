b23d1113ea97303079a21fdc02c8b4b497626ef3
Grzegorz Abramczyk <abramczykg@gmail.com>
2019-11-10 21:00:29 +0100

    Updated navigation

--- web/src/index.ts
+++ web/src/index.ts
@@ -73,12 +73,18 @@ app({router: router([
 <div class="row">
   <div class="col-3">
     <div class="nav flex-column nav-pills">
-      <router-link :to="{name:'book-list'}" class="nav-link">Books</router-link>
-      <router-link :to="{name:'author-list'}" class="nav-link">Authors</router-link>
-      <router-link :to="{name:'series-list'}" class="nav-link">Series</router-link>
-      <router-link :to="{name:'owned-list'}" class="nav-link">Owned</router-link>
-      <router-link :to="{name:'wanted-list'}" class="nav-link">Wanted</router-link>
-      <router-link :to="{name:'read-list'}" class="nav-link">Read</router-link>
+      <router-link :to="{name:'reading'}" class="nav-link">Reading</router-link>
+      <router-link :to="{name:'explore'}" class="nav-link">Explore</router-link>
+      <router-link :to="{name:'inbox'}" class="nav-link">Inbox</router-link>
+      <div class="pl-4">
+        <h3>Model</h3>
+        <router-link :to="{name:'book-list'}" class="nav-link">Books</router-link>
+        <router-link :to="{name:'author-list'}" class="nav-link">Authors</router-link>
+        <router-link :to="{name:'series-list'}" class="nav-link">Series</router-link>
+        <router-link :to="{name:'owned-list'}" class="nav-link">Owned</router-link>
+        <router-link :to="{name:'wanted-list'}" class="nav-link">Wanted</router-link>
+        <router-link :to="{name:'read-list'}" class="nav-link">Read</router-link>
+      </div>
     </div>
   </div>
   <router-view></router-view>
@@ -86,18 +92,75 @@ app({router: router([
         `
       },
       children: [
-        hg.list({name: 'book', path: 'books', fetch: api.list('books')}),
-        hg.item({name: 'book', path: 'books/:book', fetch: api.get('books')}),
-        hg.list({name: 'author', path: 'authors', fetch: api.list('authors')}),
-        hg.item({name: 'author', path: 'authors/:author', fetch: api.get('authors')}),
-        hg.list({name: 'series'}),
-        hg.item({name: 'series'}),
-        hg.list({name: 'read'}),
-        hg.item({name: 'read'}),
-        hg.list({name: 'owned'}),
-        hg.item({name: 'owned'}),
-        hg.list({name: 'wanted'}),
-        hg.item({name: 'wanted'})
+        {
+          name: 'reading',
+          path: 'reading',
+          component: {
+            template: `
+<h1>Reading</h1>
+            `
+          }
+        }, {
+          name: 'explore',
+          path: 'explore',
+          component: {
+            template: `
+<div>
+  <h1>Explore</h1>
+  <router-view></router-view>
+</div>
+            `
+          },
+          children: [
+            {
+              name: 'explore-book',
+              path: 'book',
+              component: {
+                template: `
+<h1>Book</h1>
+                `
+              }
+            }, {
+              name: 'explore-author',
+              path: 'author',
+              component: {
+                template: `
+<h1>Author</h1>
+                `
+              }
+            }
+          ]
+        }, {
+          name: 'inbox',
+          path: 'inbox',
+          component: {
+            template: `
+<h1>Inbox</h1>
+            `
+          }
+        }, {
+        name: 'model',
+        path: 'model',
+        component: {
+          template: `
+  <router-view></router-view>
+          `
+        },
+        children: [
+          hg.list({name: 'book', path: 'books', fetch: api.list('books')}),
+          hg.item({name: 'book', path: 'books/:book', fetch: api.get('books')}),
+          hg.list({name: 'author', path: 'authors', fetch: api.list('authors')}),
+          hg.item({name: 'author', path: 'authors/:author', fetch: api.get('authors')}),
+          hg.list({name: 'series'}),
+          hg.item({name: 'series'}),
+          hg.list({name: 'read'}),
+          hg.item({name: 'read'}),
+          hg.list({name: 'owned'}),
+          hg.item({name: 'owned'}),
+          hg.list({name: 'wanted'}),
+          hg.item({name: 'wanted'})
+          ]
+        }
       ]
     }
   ]
