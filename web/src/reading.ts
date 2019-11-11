a435eb4c7f436e3f481456c9b6b1ac77c03eff6a
Grzegorz Abramczyk <abramczykg@gmail.com>
2019-11-11 20:42:06 +0100

    Impreved reading display

--- web/src/reading.ts
+++ web/src/reading.ts
@@ -8,26 +8,37 @@ const config: RouteConfig = {
 <div class="col-9">
     <h3>Read</h3>
     <div class="row">
-      <div class="col-12 mt-1" v-for="book in read">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">
-              {{book.title}}
-              <small>{{book.author.name}}</small>
-            </h3>
-            <div class="progress">
-              <div class="progress-bar"
-                role="progressbar"
-                :style="{width: book.progress[0].position/book.pages*100+'%'}"
-                :aria-valuenow="book.progress[0].position"
-                aria-valuemin="0"
-                :aria-valuemax="book.pages">
-                  {{book.progress[0].position}}/{{book.pages}}
-                </div>
+      <div class="col-12 mt-4" v-for="book in read">
+        <h4>
+          {{book.title}}
+          <small>{{book.author.name}}</small>
+        </h4>
+        <div class="progress">
+          <div class="progress-bar"
+            role="progressbar"
+            :style="{width: book.progress[0].position/book.pages*100+'%'}"
+            :aria-valuenow="book.progress[0].position"
+            aria-valuemin="0"
+            :aria-valuemax="book.pages">
+              {{book.progress[0].position}}/{{book.pages}}
             </div>
-          </div>
-
         </div>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Date</th>
+              <th scope="col">Page</th>
+              <th scope="col">Progress</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="progress in book.progress">
+              <td>{{progress.date}}</td>
+              <td>{{progress.position}}</td>
+              <td><span class="badge badge-primary" v-if="progress.increment > 0">+{{progress.increment}}</span></td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
 </div>
